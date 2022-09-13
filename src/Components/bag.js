a9cd2e17543e76109b17e7d59f3fc6697cd5e39c
akbarob <akbarbadmus07@gmail.com>
2022-09-13 18:55:54 +0100

    vh

--- src/Components/bag.js
+++ src/Components/bag.js
@@ -1,6 +1,6 @@
 export default function Bag(){
     return(
-        <div className="text-center">
+        <div className="text-center mt-5 brand" style={{height:"70vh"}}>
             <h4>You have 0 item(s) in your bag</h4>
             Sign in to see your bag
 and get shopping!
